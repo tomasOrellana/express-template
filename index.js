e778f40c21cea2b9ec3aa45f695781651732320e
Tomas Orellana <tomas.orellana@bicevida.cl>
2023-02-01 14:41:24 -0300

    asd

--- index.js
+++ index.js
@@ -5,4 +5,6 @@ app.get('/api', (req, res) => {
   res.send({ message: 'Hello from Express API' });
 });
 
-app.listen(3000, () => console.log('Express server running on port 3000'));
+const port = process.env.PORT || 3000;
+
+/* app.listen(port, () => console.log('Express server running on port', port)); */
